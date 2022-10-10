08af4b40b16bd778cf574aa30b13a4b168a61c4a
S♡PHIE <ssimpson2022@elam.co.uk>
2022-10-10 15:01:41 +0100

    Parallaxing background

---
+++ RunnerUNITY/Runner/Assets/Parallax.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    GameObject player;
+    Renderer rend;
+
+    float playerStartPos;
+    public float speed = 0.05f;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        rend = GetComponent<Renderer>();
+        playerStartPos = player.transform.position.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = (player.transform.position.x - playerStartPos) * speed;
+
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0f));
+    }
+}
